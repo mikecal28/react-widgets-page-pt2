4211a399f4250675d502a22dd6d48d1e3c4df217
mikecal28 <mt28.caldwell@gmail.com>
2022-08-14 11:27:29 -0600

    finishes analogue clock widget

--- src/components/widgets/AnalogueClock.js
+++ src/components/widgets/AnalogueClock.js
@@ -3,7 +3,7 @@ import styled, { css, keyframes } from "styled-components";
 
 function AnalogueClock() {
   const [sHand, setSHand] = useState(0);
-  const [mHand, setMHand] = useState(45 * 6);
+  const [mHand, setMHand] = useState(0);
   const [hHand, setHHand] = useState(0);
 
   const seconds = keyframes`
@@ -25,9 +25,12 @@ function AnalogueClock() {
 
   const SecondHand = styled.div`
     width: 0.05rem;
-    height: 15rem;
+    height: 14rem;
     background: linear-gradient(to bottom, black 49%, transparent 50%);
     animation: ${secondsAnimation} linear infinite;
+    position: absolute;
+    top: 20.5%;
+    left: 50.5%;
   `;
 
   const minutes = keyframes`
@@ -53,15 +56,62 @@ function AnalogueClock() {
     background: linear-gradient(to bottom, black 49%, transparent 50%);
     animation: ${minutesAnimation} linear infinite;
     position: absolute;
-    top: 36%;
+    top: 24.5%;
+    left: 50%;
   `;
+
+  const hours = keyframes`
+  0% {
+    transform: rotate3d(0, 0, 1, ${hHand}deg);
+  }
+  50% {
+    transform: rotate3d(0, 0, 1, ${hHand + 180}deg);
+  }
+  100% {
+    transform: rotate3d(0, 0, 1, ${hHand + 360}deg);
+  }
+  `;
+
+  const hoursAnimation = (props) =>
+    css`
+      ${hours} 216000s linear infinite;
+    `;
+
+  const HourHand = styled.div`
+    width: 0.3rem;
+    height: 10rem;
+    background: linear-gradient(to bottom, red 49%, transparent 50%);
+    animation: ${hoursAnimation} linear infinite;
+    position: absolute;
+    top: 28%;
+    left: 49.5%;
+  `;
+
+  useEffect(() => {
+    const today = new Date();
+    const hoursOffset = (today.getMinutes() / 60) * 30;
+    const minutesOffset = (today.getSeconds() / 60) * 6;
+    setHHand(today.getHours() * 30 + hoursOffset);
+    setMHand(today.getMinutes() * 6 + minutesOffset);
+    setSHand(today.getSeconds() * 6);
+  }, []);
+
+  useEffect(() => {
+    console.log("Time: ", hHand, ":", mHand, ":", sHand);
+  }, [hHand, mHand, sHand]);
 
   return (
     <div className="analogue-clock">
       <h1>AnalogueClock</h1>
       <div className="analogue-clock-wrapper">
+        <img
+          src="./assets/clock-template-cropped.png"
+          className="clock-face"
+        ></img>
         <SecondHand />
         <MinuteHand />
+        <HourHand />
+        <div className="center-piece"></div>
       </div>
     </div>
   );
